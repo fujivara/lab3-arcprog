e513c16660bf831db910cc94ff0815e9e2fbc47e
Anna <fesun.anna@lll.kpi.ua>
2024-05-04 18:20:59 +0300

    add window with figure

--- ui/window.go
+++ ui/window.go
@@ -43,7 +43,9 @@ func (pw *Visualizer) Update(t screen.Texture) {
 
 func (pw *Visualizer) run(s screen.Screen) {
 	w, err := s.NewWindow(&screen.NewWindowOptions{
-		Title: pw.Title,
+		Title:  pw.Title,
+		Width:  800,
+		Height: 800,
 	})
 	if err != nil {
 		log.Fatal("Failed to initialize the app window:", err)
@@ -131,9 +133,30 @@ func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
 }
 
 func (pw *Visualizer) drawDefaultUI() {
-	pw.w.Fill(pw.sz.Bounds(), color.Black, draw.Src) // Фон.
+	pw.w.Fill(pw.sz.Bounds(), color.White, draw.Src) // Білий фон.
 
-	// TODO: Змінити колір фону та додати відображення фігури у вашому варіанті.
+	// Розміри квадратів.
+	squareWidth := 400
+	squareHeight := 200
+
+	// Розміри вікна.
+	winWidth := pw.sz.Bounds().Dx()
+	winHeight := pw.sz.Bounds().Dy()
+
+	// Обчислення позицій по центру вікна.
+	posX1 := (winWidth - squareWidth) / 2
+	posY1 := (winHeight - squareHeight) / 2
+
+	posX2 := (winWidth - squareHeight) / 2
+	posY2 := (winHeight - squareWidth) / 2
+
+	// Малювання першого квадрата.
+	square1 := image.Rect(posX1, posY1, posX1+squareWidth, posY1+squareHeight)
+	pw.w.Fill(square1, color.Black, draw.Src)
+
+	// Малювання другого квадрата.
+	square2 := image.Rect(posX2, posY2, posX2+squareHeight, posY2+squareWidth)
+	pw.w.Fill(square2, color.Black, draw.Src)
 
 	// Малювання білої рамки.
 	for _, br := range imageutil.Border(pw.sz.Bounds(), 10) {
