3ae085752e29edcbeab4257fd3aaeaf31d7aab9b
fujivara <marat.skyba@gmail.com>
2024-05-12 13:35:15 +0300

    feature: implemented arguments parsing

--- painter/lang/parser.go
+++ painter/lang/parser.go
@@ -1,21 +1,60 @@
 package lang
 
 import (
+	"bufio"
 	"io"
+	"strings"
 
 	"github.com/roman-mazur/architecture-lab-3/painter"
 )
 
-// Parser уміє прочитати дані з вхідного io.Reader та повернути список операцій представлені вхідним скриптом.
 type Parser struct {
+}
+
+func complexOperation(operation painter.OperationFunc) painter.Operation {
+	if operation == nil {
+		return nil
+	}
+	return operation
+}
+
+func (p *Parser) commandParser(command string, args []string) painter.Operation {
+	switch command {
+	case "white":
+		return painter.OperationFunc(painter.WhiteFill)
+	case "green":
+		return painter.OperationFunc(painter.GreenFill)
+	case "bgrect":
+		return complexOperation(painter.DrawRectangle(args))
+	case "move":
+		return complexOperation(painter.Move(args))
+	case "figure":
+		return complexOperation(painter.Figure(args))
+	case "update":
+		return painter.UpdateOp
+	case "reset":
+		return painter.OperationFunc(painter.Reset)
+	}
+	return nil
 }
 
 func (p *Parser) Parse(in io.Reader) ([]painter.Operation, error) {
 	var res []painter.Operation
 
-	// TODO: Реалізувати парсинг команд.
-	res = append(res, painter.OperationFunc(painter.WhiteFill))
-	res = append(res, painter.UpdateOp)
+	scanner := bufio.NewScanner(in)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		commandLine := scanner.Text()
+		sliced := strings.Split(commandLine, " ")
+		args := sliced[1:]
+
+		command := p.commandParser(sliced[0], args)
+
+		if command != nil {
+			res = append(res, command)
+		}
+	}
 
 	return res, nil
 }
